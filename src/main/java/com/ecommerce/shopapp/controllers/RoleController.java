85c06fc755e0d77045d464b5069dd98ddd419379
sinatranguyen <kiennv.contact@gmail.com>
2024-09-20 14:41:26 +0700

    done roles

---
+++ src/main/java/com/ecommerce/shopapp/controllers/RoleController.java
@@ -0,0 +1,32 @@
+package com.ecommerce.shopapp.controllers;
+
+
+import com.ecommerce.shopapp.entity.Role;
+import com.ecommerce.shopapp.responses.ResponseObject;
+import com.ecommerce.shopapp.services.impl.RoleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("${api.prefix}/roles")
+@RequiredArgsConstructor
+@CrossOrigin("*")
+public class RoleController {
+    private final RoleService roleService;
+    @GetMapping("")
+    public ResponseEntity<ResponseObject> getAllRoles() {
+        List<Role> roles = roleService.getAllRoles();
+        return ResponseEntity.ok().body(ResponseObject.builder()
+                .message("Get roles successfully")
+                .status(HttpStatus.OK)
+                .data(roles)
+                .build());
+    }
+}
---
+++ src/main/java/com/ecommerce/shopapp/services/IRoleService.java
@@ -0,0 +1,10 @@
+package com.ecommerce.shopapp.services;
+
+
+import com.ecommerce.shopapp.entity.Role;
+
+import java.util.List;
+
+public interface IRoleService {
+    List<Role> getAllRoles();
+}
