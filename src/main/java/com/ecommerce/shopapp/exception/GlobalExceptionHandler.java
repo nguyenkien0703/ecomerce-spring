37aa06cb637af198d73d893fe97cd046a4909932
sinatranguyen <kiennv.contact@gmail.com>
2024-09-23 10:20:44 +0700

    fix: processing fix migrate db

--- src/main/java/com/ecommerce/shopapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ecommerce/shopapp/exception/GlobalExceptionHandler.java
@@ -3,13 +3,9 @@ package com.ecommerce.shopapp.exception;
 import com.ecommerce.shopapp.responses.ResponseObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.Date;
 
 /**
  * @author Admin
